60c712e62fc3aab2073e5537e7677de3865464f1
Anand R <graceanand99@gmail.com>
2023-06-11 21:43:10 +0530

    Added vue and react

---
+++ src/components/react/Header/index.tsx
@@ -0,0 +1,13 @@
+import styles from "./styles.module.css"
+
+type Props = {}
+
+function Header({}: Props) {
+  return (
+    <header className={styles.header}>
+      Header
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/vue/footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="footer">
+    <p>Footer</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Footer',
+});
+
+</script>
+
+<style scoped>
+.footer {
+  background-color: #f0f0f0;
+  padding: 20px;
+  text-align: center;
+}
+</style>
